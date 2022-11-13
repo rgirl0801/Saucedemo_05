0f60c0eeaeb8f5e11da2d3d563129ed74aa02bd0
tmp7125 <not_exist@not_exist.com>
2022-11-12 22:19:16 -0500

    Updated locators to use tuples

--- locators/login_locators.py
+++ locators/login_locators.py
@@ -2,9 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class LoginLocators:
-    input_username = {'by': By.ID, 'v': 'user-name'}
-    input_password = {'by': By.ID, 'v': 'password'}
-    login_btn = {'by': By.ID, 'v': 'login-button'}
-    hamburger_btn = {'by': By.ID, 'v': 'react-burger-menu-btn'}
-    logout_btn = {'by': By.ID, 'v': 'logout_sidebar_link'}
+    input_username = (By.ID, 'user-name')
+    input_password = (By.ID, 'password')
+    login_btn = (By.ID, 'login-button')
+    hamburger_btn = (By.ID, 'react-burger-menu-btn')
+    logout_btn = (By.ID, 'logout_sidebar_link')
     title = 'Swag Labs'
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,14 +9,10 @@ class LoginPage:
         return self.driver.title
 
     def action_login(self, username, password):
-        self.driver.find_element(
-            ll.input_username['by'], ll.input_username['v']
-        ).send_keys(username)
-        self.driver.find_element(
-            ll.input_password['by'], ll.input_password['v']
-        ).send_keys(password)
-        self.driver.find_element(ll.login_btn['by'], ll.login_btn['v']).click()
+        self.driver.find_element(*ll.input_username).send_keys(username)
+        self.driver.find_element(*ll.input_password).send_keys(password)
+        self.driver.find_element(*ll.login_btn).click()
 
     def action_logout(self):
-        self.driver.find_element(ll.hamburger_btn['by'], ll.hamburger_btn['v']).click()
-        self.driver.find_element(ll.logout_btn['by'], ll.logout_btn['v']).click()
+        self.driver.find_element(*ll.hamburger_btn).click()
+        self.driver.find_element(*ll.logout_btn).click()
