7ba6c89e258703916e5eae0f0f90c548072bab31
tmp7125 <not_exist@not_exist.com>
2022-11-18 17:48:54 -0500

    Added timeout as an arg to methods of core

--- core/core.py
+++ core/core.py
@@ -10,23 +10,23 @@ class Core:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, locator):
+    def find_element(self, locator, wait=5):
         try:
             self.LOGGER.info("local core.find_element method")
-            return WebDriverWait(self.driver, timeout=5).until(
+            return WebDriverWait(self.driver, timeout=wait).until(
                 lambda d: d.find_element(locator[0], locator[1])
             )
         except NoSuchElementException as e:
             self.LOGGER.error(f"NoSuchElementException: {e}")
 
-    def send_keys(self, locator, value):
+    def send_keys(self, locator, value, wait=5):
         try:
-            self.find_element(locator).send_keys(value)
+            self.find_element(locator, wait).send_keys(value)
         except Exception as e:
             self.LOGGER.error(f"Exception: {e}")
 
-    def click(self, locator):
+    def click(self, locator, wait=5):
         try:
-            self.find_element(locator).click()
+            self.find_element(locator, wait).click()
         except Exception as e:
             self.LOGGER.error(f"Exception: {e}")
